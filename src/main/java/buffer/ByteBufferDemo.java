d439c4056895ced787e9a019d6edb3764f54e012
liuwei <wei.liu@shmedo.cn>
2022-08-09 10:29:00 +0800

    add buffer

---
+++ src/main/java/buffer/ByteBufferDemo.java
@@ -0,0 +1,50 @@
+package buffer;
+
+import java.nio.ByteBuffer;
+
+public class ByteBufferDemo {
+    public static void main(String[] args) {
+        // create buffer
+        System.out.println("create buffer");
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        printBuffer(buffer);
+
+        //add data to buffer
+        System.out.println("add data to buffer");
+        String s = "h2pl4u";
+        buffer.put(s.getBytes());
+        printBuffer(buffer);
+
+        //check read
+        System.out.println("check read");
+        buffer.flip();
+        printBuffer(buffer);
+
+        // read data
+        System.out.println("read data");
+        byte[] bytes = new byte[buffer.limit()];
+        buffer.get(bytes);
+        printBuffer(buffer);
+
+        //compact
+        System.out.println("compact");
+        buffer.compact();
+        printBuffer(buffer);
+
+        //clear
+        System.out.println("clear");
+        buffer.clear();
+        printBuffer(buffer);
+    }
+
+    /**
+     * print buffer info
+     * @param buffer
+     */
+    private static void printBuffer(ByteBuffer buffer) {
+        System.out.println("mark:" + buffer.mark());
+        System.out.println("position:" + buffer.position());
+        System.out.println("limit:" + buffer.limit());
+        System.out.println("capacity:" + buffer.capacity());
+    }
+}
